59f9b64f43d483b93789fb6db84045ed657dfac8
MadushaniR <madushani.2019360@iit.ac.lk>
2024-10-02 17:39:58 +0530

    data added

--- src/app/client-management/client-management.component.ts
+++ src/app/client-management/client-management.component.ts
@@ -1,12 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-client-management',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatBadgeModule
+  ],
   templateUrl: './client-management.component.html',
-  styleUrl: './client-management.component.css'
+  styleUrls: ['./client-management.component.css'], // Corrected here
 })
 export class ClientManagementComponent {
-
+  employees = [
+    {
+      name: 'Sanjaya Perera',
+      position: 'UI UX Designer',
+      category: 'Permanent',
+      joinDate: '01 Sep 2023',
+      location: 'Onsite',
+      locationIcon: 'location_on',
+      image: 'path_to_image',
+    },
+    {
+      name: 'Kierra Culhane',
+      position: 'Accountant',
+      category: 'Permanent',
+      joinDate: '01 Sep 2023',
+      location: 'Hybrid',
+      locationIcon: 'location_on',
+      image: 'path_to_image',
+    },
+    // Add more employee data as needed
+  ];
 }
